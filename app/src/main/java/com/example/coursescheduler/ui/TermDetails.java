6ef872b640286307e6f7d0d496bf2f0732cf487d
FastLikeAstar <fastlikeastar@gmail.com>
2022-12-14 11:24:30 -0700

    Added each required activity. (High level - activities functionality not implemented yet)

---
+++ app/src/main/java/com/example/coursescheduler/ui/TermDetails.java
@@ -0,0 +1,16 @@
+package com.example.coursescheduler.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.coursescheduler.R;
+
+public class TermDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_details);
+    }
+}
