6c5edfd0c5d74bcf6cee4ef73e2b0dabdd5b2b01
FastLikeAstar <fastlikeastar@gmail.com>
2022-12-14 12:11:17 -0700

    Began navigation between pages. Will develop further after Activity UIs are created.

--- app/src/main/java/com/example/coursescheduler/ui/TermList.java
+++ app/src/main/java/com/example/coursescheduler/ui/TermList.java
@@ -3,6 +3,8 @@ package com.example.coursescheduler.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.coursescheduler.R;
 
@@ -12,5 +14,20 @@ public class TermList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_term_list, menu);
+        return true;
+    }
+
+    public boolean opOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
